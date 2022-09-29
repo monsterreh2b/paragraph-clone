fd9a496a909252f65df646b6986cc8185e56afb7
Brant Han <sghan@jcehrlich.local>
2022-09-29 12:11:23 -0700

    testing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var paragraphTrain = [];
 var firstGuy = document.getElementById("original");
 var parent = document.getElementById("parent");
+var test = document.getElementById("test");
 
-for (var i = 0; i < 7; i++) {
+console.log(test);
+
+/*for (var i = 0; i < 7; i++) {
     paragraphTrain.push(firstGuy);
     
  
@@ -12,10 +15,46 @@ console.log(parent);
 console.log(firstGuy);
 
 console.log(paragraphTrain[0].innerHTML);
+console.log(paragraphTrain[0].parentNode);
 
 for (var i = 0; i < 7; i++) {
     parent.append(paragraphTrain[i].innerHTML);
-}
+}*/
+
+
+var clones = [];
+var clone = firstGuy.cloneNode(true);
+var clone2 = firstGuy.cloneNode(true);
+var clone3 = firstGuy.cloneNode(true);
+var clone4 = firstGuy.cloneNode(true);
+var clone5 = firstGuy.cloneNode(true);
+var clone6 = firstGuy.cloneNode(true);
+var clone7 = firstGuy.cloneNode(true);
+
+clones.push(clone);
+
+console.log(clones[0]);
+
+parent.append(clone);
+parent.append(clone2);
+parent.append(clone3);
+parent.append(clone4);
+parent.append(clone5);
+parent.append(clone6);
+parent.append(clone7);
+
+//parent.append(clones[0]);
+
+var clonesFin = [];
+var cloneFin = firstGuy.cloneNode(true);
+clonesFin.push(cloneFin);
+console.log(cloneFin);
+
+
+
+
+
+
 
 
 
